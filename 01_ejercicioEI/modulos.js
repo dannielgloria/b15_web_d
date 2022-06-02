c592b64e75ef06dea2fc418e7ad5b33ba30febaa
dannielgloria <danniel.gloria@gmail.com>
2022-06-01 19:35:35 -0500

    ejercicio 1

---
+++ 01_ejercicioEI/modulos.js
@@ -0,0 +1,14 @@
+/* A function that prints the name */
+var getName = function(name){
+    console.log('Nombre: '+name)
+    return 
+};
+
+/* A function that prints the name of a tv star */
+var getStar = function (star) {
+    console.log('Personaje de tv: ' + star)
+    return 
+}
+
+/* Exporting the functions getName and getStar to be used in other files. */
+module.exports = {getName,getStar};
